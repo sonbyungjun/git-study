eac406ee2bbef69834623846588d7332d1738f6b
sonbyungjun <byungjun2050@gmail.com>
2019-01-04 21:42:05 +0900

    branch test

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res, next) => {
     res.end('Hello byungjun!')
 })
 
-app.listen(8080, function(err) {
+app.get('/hello', (req, res, next) => {
+    res.end("Hello Hello")
+})
+
+app.listen(8080, (err) => {
     console.log("server running...")
 })
